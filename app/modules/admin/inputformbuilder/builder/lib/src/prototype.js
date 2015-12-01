f1214732c1467ad97807ea9afd8f4e1e129f2c1e
Abhishek Shende <abhishekisnot@gmail.com>
2015-12-01 00:54:28 +0000

    remove builder related files from repo

--- app/modules/admin/inputformbuilder/builder/lib/src/prototype.js
+++
@@ -1,8 +0,0 @@
-﻿/*jshint -W121 */
-/*TODo: Although I (Xiaomin, s of 06/01/2015) ignore jshint warning in above, the following is not a good practice */
-/*Need to review later.*/
-String.prototype.replaceAt = function (index, character) {
-  'use strict';
-  return this.substr(0, index) + character + this.substr(index + character.length);
-};
-
